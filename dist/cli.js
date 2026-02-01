2f6e875d27f9cc858a61c336c8ca64aa2df0189f
Peter Skoett <peterskoett@gmail.com>
2026-02-01 20:19:19 +0100

    Add CLI banner and version output

--- dist/cli.js
+++ dist/cli.js
@@ -1,15 +1,30 @@
 #!/usr/bin/env node
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
 const scan_1 = require("./commands/scan");
 const report_1 = require("./commands/report");
 const agent_1 = require("./services/agent");
+const chalk_1 = __importDefault(require("chalk"));
+const BANNER = [
+    '   ___            _            _     _____                         ',
+    '  / __\\___  _ __ | |_ _____  _| |_  |  ___| __ __ _ _ __ ___   ___ ',
+    " / /  / _ \\| '_ \\| __/ _ \\ \\/ / __| | |_ | '__/ _` | '_ ` _ \\ / _ \\",
+    '/ /__| (_) | | | | ||  __/>  <| |_  |  _|| | | (_| | | | | | |  __/',
+    '\\____/\\___/|_| |_|\\__\\___/_/\\_\\\\__| |_|  |_|  \\__,_|_| |_| |_|\\___|'
+].join('\n');
 const program = new commander_1.Command();
 program
     .name('context-frame')
     .description('Measure AI context maturity in your codebase')
     .version('1.0.0');
+program.hook('preAction', (thisCommand) => {
+    console.log(chalk_1.default.cyan(BANNER));
+    console.log(chalk_1.default.gray(`Version ${thisCommand.version()}\n`));
+});
 program
     .argument('[path]', 'Path to repository', '.')
     .action(async (targetPath) => {
--- src/cli.ts
+++ src/cli.ts
@@ -4,6 +4,15 @@ import { Command } from 'commander';
 import { scanCommand } from './commands/scan';
 import { reportCommand, ReportFormat } from './commands/report';
 import { runAgentFlow } from './services/agent';
+import chalk from 'chalk';
+
+const BANNER = [
+  '   ___            _            _     _____                         ',
+  '  / __\\___  _ __ | |_ _____  _| |_  |  ___| __ __ _ _ __ ___   ___ ',
+  " / /  / _ \\| '_ \\| __/ _ \\ \\/ / __| | |_ | '__/ _` | '_ ` _ \\ / _ \\",
+  '/ /__| (_) | | | | ||  __/>  <| |_  |  _|| | | (_| | | | | | |  __/',
+  '\\____/\\___/|_| |_|\\__\\___/_/\\_\\\\__| |_|  |_|  \\__,_|_| |_| |_|\\___|'
+].join('\n');
 
 const program = new Command();
 
@@ -11,6 +20,11 @@ program
   .name('context-frame')
   .description('Measure AI context maturity in your codebase')
   .version('1.0.0');
+
+program.hook('preAction', (thisCommand) => {
+  console.log(chalk.cyan(BANNER));
+  console.log(chalk.gray(`Version ${thisCommand.version()}\n`));
+});
 
 program
   .argument('[path]', 'Path to repository', '.')
